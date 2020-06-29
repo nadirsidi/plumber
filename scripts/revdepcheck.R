c118101b254128d7c12a52c734f31e01684e8785
Nadir Sidi <nadir.sidi@enel.com>
2020-06-29 15:51:07 -0400

    Removed a bunch of stuff to try to get plumber less than 50 MB compressed

--- scripts/revdepcheck.R
+++
@@ -1,6 +0,0 @@
-source("scripts/git_clean.R")
-
-if (!require("revdepcheck")) remotes::install_github("r-lib/revdepcheck")
-
-# revdepcheck::revdep_reset()
-revdepcheck::revdep_check(num_workers = 4)
